8df90045b497c6653eebe11eca85828d8406e051
Cuauhtémoc <tmok.silva@gmail.com>
2021-10-05 14:51:56 -0500

    project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { LocalNotifications,ELocalNotificationTriggerUnit } from '@ionic-native/local-notifications/ngx';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  constructor( private localNotifications: LocalNotifications) {}
+
+  notification( minutes: number){
+    console.log('Notificación cargado', minutes);
+    this.localNotifications.schedule({
+      title: `TíTULO DE LA NOTIFICACIÓN`,
+      text: 'Hola, este es el texto de la notificación  ',
+      trigger: {
+        in: minutes,
+        unit: ELocalNotificationTriggerUnit.MINUTE,
+        count: 5,
+      },
+      foreground: true
+    });
+  }
 
 }
